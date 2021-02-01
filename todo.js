452c5eb492876bc37d59667fd476ab87b725d0a7
Olumba solomon <olumbasolomon08@gmail.com>
2021-02-01 06:49:55 +0100

    resolved all problems with the delete button

--- todo.js
+++ todo.js
@@ -7,8 +7,8 @@ var greetingPara = document.getElementById("greeting");
 let histPara = document.getElementById("historyPara");
 
 class TodoItem {
-    constructor(text){
-        this.text= text;
+    constructor(textItem){
+        this.textItem = textItem;
     }
 }
 
@@ -23,7 +23,7 @@ class todoStorage {
         }
         return todos;
     }
-
+// method to add todos to storage 
     static addTodos(todo){
         const todos = todoStorage.getTodos();
         todos.push(todo);
@@ -31,28 +31,27 @@ class todoStorage {
         localStorage.setItem('todos', JSON.stringify(todos));
     }
 
-    //method to remove todos from local storage
-    static removeTodos(text){
+    //method to remove single todos from local storage
+    static removeTodos(texts){
         const todos = todoStorage.getTodos();
         todos.forEach((todo, index) => {
-            if(todo.text === text){
+            if(todo.textItem === texts){
                 todos.splice(index, 1);
+                console.log("a match " + todo.textItem + " " + texts);
+            }else {
+                console.log("not a match " + todo.textItem + "        " + texts)
             }
         })
         //we stringify our books array because ls doesn't take in anything that's not a string 
         localStorage.setItem('todos', JSON.stringify(todos));
-    };
+    }
 
     //method to remove all todos from todoStorage
     static removeAllTodos(){
         //get our list of todos from local storage
         let todos = JSON.parse(localStorage.getItem('todos'));
-        todos.forEach((todo, index)=>{
-            todos.splice(0, todos.length);
-        });
+        todos.splice(0, todos.length);
         localStorage.setItem('todos', JSON.stringify(todos));
-        console.log(todos)
-        
     }
 }
 
@@ -66,8 +65,8 @@ class UI {
 
     static addTodoToList(todo){
         let newItem = document.createElement("p");
-        newItem.innerHTML = `${todo.text}`;
-        newItem.className = 'newItem'
+        newItem.innerText = `${todo.textItem}`;
+        newItem.className = 'newItem';
         items.appendChild(newItem);
         let opt = document.createElement("div");
         opt.style.width = "5%";
@@ -75,22 +74,18 @@ class UI {
         //Clearing input 
         input.value = "";
         //creating a delete button for each list item created 
-        let delbtn = document.createElement("span");
-        let delbtntxtnd = document.createElement("span");
-        delbtntxtnd.className = "material-icons md-16"
-        let spanText = document.createTextNode("delete");
-        delbtntxtnd.appendChild(spanText)
+        let delbtn = document.createElement("button");
+        let delbtntxtnd = document.createElement("i");
+        delbtntxtnd.className = 'fas fa-trash';
         delbtn.appendChild(delbtntxtnd);
         delbtn.className = 'deleteButton';
         histPara.className = "histParaVisible";
         opt.appendChild(delbtn);
 
         //creating an edit button for each list item created 
-        let editbtn = document.createElement("span");
-        let editbtntxtnd = document.createElement("span");
-        editbtntxtnd.className = "material-icons md-16"
-        let editSpanText = document.createTextNode("edit");
-        editbtntxtnd .appendChild(editSpanText)
+        let editbtn = document.createElement("button");
+        let editbtntxtnd = document.createElement("i");
+        editbtntxtnd.className = "fas fa-edit";
         editbtn.appendChild(editbtntxtnd);
         editbtn.className = 'editButton';
         histPara.className = "histParaVisible";
@@ -104,7 +99,15 @@ class UI {
             newp.innerHTML = editText;
             newItem.firstChild.remove();
             // add an if to verify empty edits 
-            newItem.insertBefore(newp, opt)
+            newItem.insertBefore(newp, opt);
+        }
+
+        //function to delete individual items
+        delbtn.onclick = ()=>{
+           delbtn.parentElement.parentElement.remove();
+           let textItem = delbtn.parentElement.parentElement.textContent;
+           todoStorage.removeTodos(textItem);
+           if(items.children.length < 1) histPara.className = 'historyPara';
         }
     
     }
@@ -133,4 +136,3 @@ addBtn.addEventListener("click", ()=>{
      histPara.className = 'historyPara';
      todoStorage.removeAllTodos();
 });
-
